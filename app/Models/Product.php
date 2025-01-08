62903a06bd4feb38897404d6c311314c0d824ca0
svitorz <vitor.fabio.castro@gmail.com>
2025-01-08 13:39:05 -0300

    add products table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'description', 'price', 'stock'
+    ];
+
+    public function categories(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\Category::class);
+    }
 }
