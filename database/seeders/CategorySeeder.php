62571439e7e3bb3712efa0412aff5399b4fd2f84
svitorz <vitor.fabio.castro@gmail.com>
2025-01-08 13:41:32 -0300

    add seeder and factory to category class

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\CategoryFactory;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -12,6 +12,6 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        (new CategoryFactory)->count(10)->create();
     }
 }
